2e99737cae32032aa55fd3878cd0de9827ce7b49
jayetl27 <jayetl27@gmail.com>
2018-10-09 23:51:18 +0530

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "test09102018"
+
+version := "0.1"
+
+scalaVersion := "2.11.7"
---
+++ src/main/scala/org/jay/scalatest/scalatest.scala
@@ -0,0 +1,10 @@
+package org.jay.scalatest
+
+object scalatest {
+
+  def main(args: Array[String]): Unit = {
+
+    println("jay")
+  }
+
+}
